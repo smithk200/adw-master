2da14c06463e7c0df7226cad3cc14d1f9cfbaae0
smithk200 <smithk56789@gmail.com>
2025-05-22 00:01:23 -0500

    more intro stuff

--- src/field_specials.c
+++ src/field_specials.c
@@ -1208,7 +1208,7 @@ void EndLotteryCornerComputerEffect(void)
 void SetTrickHouseNuggetFlag(void)
 {
     u16 *specVar = &gSpecialVar_0x8004;
-    u16 flag = FLAG_HIDDEN_ITEM_TRICK_HOUSE_NUGGET;
+    u16 flag = FLAG_FILLER_TRICK_HOUSE_FLAG;
     *specVar = flag;
     FlagSet(flag);
 }
@@ -1216,7 +1216,7 @@ void SetTrickHouseNuggetFlag(void)
 void ResetTrickHouseNuggetFlag(void)
 {
     u16 *specVar = &gSpecialVar_0x8004;
-    u16 flag = FLAG_HIDDEN_ITEM_TRICK_HOUSE_NUGGET;
+    u16 flag = FLAG_FILLER_TRICK_HOUSE_FLAG;
     *specVar = flag;
     FlagClear(flag);
 }
